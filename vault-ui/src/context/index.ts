1eafb88adebc74de89ee806d50d628dca0123e09
AdekunleBamz <adekunlebamz@gmail.com>
2026-01-14 23:13:46 +0100

    feat(context): add context barrel export

---
+++ vault-ui/src/context/index.ts
@@ -0,0 +1,3 @@
+export { ToastProvider, useToast, type Toast, type ToastType } from './ToastContext';
+export { AppProvider, useAppContext, useTheme, useSidebar, useAppLoading, type AppState, type AppAction } from './AppContext';
+export { ModalProvider, useModal, useModalState } from './ModalContext';
