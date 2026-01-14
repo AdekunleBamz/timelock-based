83bb47ef9efe8e408280a85f3b559706aab6f487
AdekunleBamz <adekunlebamz@gmail.com>
2026-01-14 22:53:34 +0100

    refactor(hooks): organize and export all custom hooks

--- vault-ui/src/hooks/index.ts
+++ vault-ui/src/hooks/index.ts
@@ -1,8 +1,17 @@
+// Core app hooks
 export { useWallet } from "./useWallet";
 export { useVault } from "./useVault";
+
+// State management hooks
 export { useLocalStorage } from "./useLocalStorage";
+export { useToggle, useDisclosure } from "./useToggle";
+export { usePrevious, useHasChanged } from "./usePrevious";
+
+// Timing hooks
 export { useDebounce, useDebouncedCallback } from "./useDebounce";
 export { useInterval, useControllableInterval } from "./useInterval";
+
+// UI utility hooks
 export { 
   useMediaQuery, 
   useIsMobile, 
@@ -11,3 +20,8 @@ export {
   usePrefersReducedMotion,
   usePrefersDarkMode 
 } from "./useMediaQuery";
+export { useOnClickOutside } from "./useOnClickOutside";
+export { useClipboard } from "./useClipboard";
+
+// Async hooks
+export { useAsync } from "./useAsync";
