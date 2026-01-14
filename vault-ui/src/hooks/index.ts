439bd1805f810267366bcd8c2631a7f8e718c108
AdekunleBamz <adekunlebamz@gmail.com>
2026-01-14 22:38:11 +0100

    refactor(hooks): export all custom hooks from barrel file

--- vault-ui/src/hooks/index.ts
+++ vault-ui/src/hooks/index.ts
@@ -1,2 +1,13 @@
 export { useWallet } from "./useWallet";
 export { useVault } from "./useVault";
+export { useLocalStorage } from "./useLocalStorage";
+export { useDebounce, useDebouncedCallback } from "./useDebounce";
+export { useInterval, useControllableInterval } from "./useInterval";
+export { 
+  useMediaQuery, 
+  useIsMobile, 
+  useIsTablet, 
+  useIsDesktop, 
+  usePrefersReducedMotion,
+  usePrefersDarkMode 
+} from "./useMediaQuery";
